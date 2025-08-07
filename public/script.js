df321c6528c0dc054a96d311946003c17499980f
saracaitlyn-cruz11 <b2029scacruz@pshs.edu.ph>
2025-08-07 10:16:58 +0800

    java

---
+++ public/script.js
@@ -0,0 +1,7 @@
+let age = 14;
+let maxAge = 70;
+let numPerDay = 2;
+
+totalRequired = (numPerDay*365)*(maxAge-age);
+
+document.write("You will need "+totalRequired + " cookies until the ripe old age of "+maxAge);    
